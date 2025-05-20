a76420739dd637719f50f3778c129a9effd4fed0
Soffivp <psvire@utpl.edu.ec>
2025-05-20 09:27:41 -0500

    primeros cambios

---
+++ ejemplo02/ingresa_data.py
@@ -0,0 +1,24 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# se importa la clase(s) del 
+# archivo genera_tablas
+from genera_tablas import Club, Jugador
+
+# se importa información del archivo configuracion
+from configuracion import cadena_base_datos
+# se genera enlace al gestor de base de
+# datos
+# para el ejemplo se usa la base de datos
+# sqlite
+engine = create_engine(cadena_base_datos)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+open("data/datos_clubs.txt")
+
+with open("data/datos_clubs.txt") as archivo:
+     for linea in archivo:
+        print(linea)
